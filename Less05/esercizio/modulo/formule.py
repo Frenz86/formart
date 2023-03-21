a1ba8ec6cb0cb55ad007135bda6fe47459e8dd8e
Frenz86 <frenzy86@gmail.com>
2023-03-21 13:54:28 +0100

    less 05

---
+++ Less05/esercizio/modulo/formule.py
@@ -0,0 +1,9 @@
+def area_rettangolo1(a:float,b:float):
+    ''' area rettangolo'''
+    area = a*b
+    return area
+
+def per_rettangolo1(a:float,b:float):
+    ''' perimetro rettangolo'''
+    per = 2*(a+b)
+    return per
