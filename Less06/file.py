ebf428c3d706d2b9a2da7b125335a6ab522170da
Frenz86 <frenzy86@gmail.com>
2023-03-23 19:04:32 +0100

    clean

---
+++ Less06/file.py
@@ -0,0 +1,14 @@
+
+def somma(a:float,b:float):
+    ''' questa funct blabla bla'''
+    s=a+b
+    print(s)
+    return s
+
+def main():
+    a=5
+    b=6
+    somma(a,b)
+
+if __name__ == "__main__":
+    main()
