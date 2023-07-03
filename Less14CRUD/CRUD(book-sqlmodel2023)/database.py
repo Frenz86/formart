b4a959c17ee64dafaec7d840c7421907da0d6ed1
Frenz86 <frenzy86@gmail.com>
2023-07-03 18:11:24 +0200

    ww

--- Less14CRUD/CRUD(book-sqlmodel2023)/database.py
+++ Less14CRUD/CRUD(book-sqlmodel2023)/database.py
@@ -1,18 +1,18 @@
 from sqlmodel import SQLModel,create_engine
 import os
 
-# BASE_DIR=os.path.dirname(os.path.realpath(__file__))
-# DB_URL = 'sqlite:///'+BASE_DIR+'/books.db'
-# conn_str = 'sqlite:///'+os.path.join(BASE_DIR,'books.db')
+BASE_DIR=os.path.dirname(os.path.realpath(__file__))
+DB_URL = 'sqlite:///'+BASE_DIR+'/books.db'
+conn_str = 'sqlite:///'+os.path.join(BASE_DIR,'books.db')
 
 #DATABASE_URL = "postgresql://user:password@postgresserver/db"
 #conn_str = "postgresql://user:password@postgresserver/db" #local
 
-from dotenv import load_dotenv
-load_dotenv()#load .env
-password : str = os.getenv('PASSWORD')
-db_id : str = os.getenv('DBID')
-conn_str = f"postgresql://postgres:{password}@db.{db_id}.supabase.co:6543/postgres"
+# from dotenv import load_dotenv
+# load_dotenv()#load .env
+# password : str = os.getenv('PASSWORD')
+# db_id : str = os.getenv('DBID')
+# conn_str = f"postgresql://postgres:{password}@db.{db_id}.supabase.co:6543/postgres"
 
 print(conn_str)
 
---
+++ Less14CRUD/template_streamlit_volume/app/app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def main():
+    st.title('first app')
+
+if __name__ =="__main__":
+    main()
